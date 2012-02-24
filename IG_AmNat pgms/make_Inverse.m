d14a8502242c36197361b21e1c7ba7d60ee4d723
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-02-23 19:10:30 -0800

    modularizing

---
+++ IG_AmNat pgms/make_Inverse.m
@@ -0,0 +1,21 @@
+function invV=make_Inverse(parameter)
+
+global initVh initVp n p q X tau1 tau2
+
+d1=abs(parameter(1));
+d2=abs(parameter(2));
+
+first_param=d1
+second_param=d2
+
+% calculate the covariance matrices under the OU process (eq A.1)
+Vh=(d1.^tau1).*(1-d1.^(2*initVh))./(1-d1^2);
+Vp=(d2.^tau2).*(1-d2.^(2*initVp))./(1-d2^2);
+
+% standardize the covariance matrices to have determinant = 1 (this avoids
+% numerical issues of determinants going to infinity or zero)
+Vh=Vh./det(Vh)^(1/p);
+Vp=Vp./det(Vp)^(1/q);
+V=kron(Vp,Vh);
+
+invV=V\eye(n);
---
+++ IG_AmNat pgms/make_MSE.m
@@ -0,0 +1,13 @@
+function MSE=make_MSE(parameter)
+
+global initVh initVp n p q X tau1 tau2
+
+invV=parameter(1);
+
+% compute the phylogenetic mean (eq. A.2)
+U=ones(length(X),1);
+b=(U'*invV*U)\(U'*invV*X);
+
+% compute the phylogenetic MSE (eq. A.2)
+H=X-b;
+MSE=(H'*invV*H)/(n-1);
