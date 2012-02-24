fa1d23081535b9952d45cafe2bb73b31a046f1e4
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-02-23 19:12:23 -0800

    calling function

--- IG_AmNat pgms/IGfunct.m
+++ IG_AmNat pgms/IGfunct.m
@@ -1,1 +1,1 @@
-% Computes the MSE for the model (eq 2)%	A = Xb + ?function MSE=IGfunct(parameter)global initVh initVp n p q X tau1 tau2d1=parameter(1);d2=parameter(2);invV=make_Inverse(d1 d2);MSE=make_MSE(invV);
+% Computes the MSE for the model (eq 2)%	A = Xb + ?function MSE=IGfunct(parameter)global initVh initVp n p q X tau1 tau2d1=parameter(1);d2=parameter(2);invV=make_Inverse(d1,d2);MSE=make_MSE(invV);
