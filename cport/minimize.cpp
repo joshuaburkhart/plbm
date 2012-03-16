9bec2f691bec508013c59515ad248ebf1ed13f78
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-16 14:25:24 -0700

    removing initial vals

--- cport/minimize.cpp
+++ cport/minimize.cpp
@@ -11,9 +11,9 @@ double *initVp;
 double *X;
 double *tau1;
 double *tau2;
-double n=324;
-double p=12;
-double q=27;
+double n;
+double p;
+double q;
 
 double funct(double *d1_d2);
 void nelmin ( double fn ( double x[] ), int n, double start[], double xmin[],double *ynewlo, double reqmin, double step[], int konvge, int kcount,int *icount, int *numres, int *ifault );
