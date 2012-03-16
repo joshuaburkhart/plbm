faedd955a10fb3fa37fca483997c74fa4b9f65dc
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-15 19:01:14 -0700

    adding nan test

---
+++ nan_test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc,int argv[]) {
+    int nan = 0 / argv[1]; // user is expected to supply a 0
+    int i;
+    for(i = -1; i < 2; i++) {
+        if(nan > i) {
+            printf("nan is > %i\n",i);
+        }
+        if(nan < i) {
+            printf("nan is < %i\n",i);
+        }
+        if(nan == i) {
+            printf("nan is == %i\n",i);
+        }
+        if(nan != i) {
+            printf("nan is != %i\n",i);
+        }
+    }
+    return 0;
+}
