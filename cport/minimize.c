711555644f808948e021cdbd9144e17ce559a880
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-16 09:04:23 -0700

    fixing typo

--- cport/minimize.c
+++ cport/minimize.c
@@ -36,9 +36,9 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[]){
     /*also try n=(int) mxGetScalar(prhs[2]); */
     p=*mxGetPr(prhs[3]); /* should be 1 x 1 matrix */
     q=*mxGetPr(prhs[4]); /* should be 1 x 1 matrix */
-    X=mexGetPr(prhs[5]);
-    tau1=mxGetPr(prhs[6]);
-    tau2=mxGetPr(prhs[7]);
+    X=*mexGetPr(prhs[5]);
+    tau1=*mxGetPr(prhs[6]);
+    tau2=*mxGetPr(prhs[7]);
 
     mexPrintf("n: %f\n",n);
     mexPrintf("p: %f\n",p);
