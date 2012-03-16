58e3420ae3945c784aed2a3ea5e3b1b7c96c9b9f
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-03-15 23:31:23 -0700

    ..

--- cport/minimize.c
+++ cport/minimize.c
@@ -26,12 +26,12 @@ void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[]);
 void mexFunction(int nlhs,mxArray *plhs[],int nrhs,const mxArray *prhs[]){
     /*expect phrs-> 0=initVh, 1=initVp, 2=n, 3=p, 4=q, 5=tau1, 6=tau2, 7=d1_d2*/
     
-    //TODO: n, p, and q may not be necessary as they might already be included in dims of other vars..?
-    //http://cnx.org/content/m12348/latest/
+    /*TODO: n, p, and q may not be necessary as they might already be included in dims of other vars..?*/
+    /* http://cnx.org/content/m12348/latest/ */
 
-    /////////////////////
-    //convert matlab vals
-    /////////////////////
+    /*///////////////////*/
+    /*convert matlab vals*
+    /*///////////////////*/
 	
     initVh=mxGetPr(prhs[0]);
     initVp=mxGetPr(prhs[1]);
