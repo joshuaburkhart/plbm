718c038f81d8c37be831e5485ef670332a6fcf6f
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-04-26 12:49:39 -0700

    formatting with astyle

--- cport/test_main.cpp
+++ cport/test_main.cpp
@@ -38,15 +38,15 @@ int main(int argc,char *argv[]) {
     double expect_vh_5_5[]= {1.1776,0.7765,0,0.1629,0.3000,0.1629,0.4960,0.0670,0.0670,0.0670,0.4960,0.0670,0.7765,1.1776,0,0.1629,0.3000,0.1629,0.4960,0.0670,0.0670,0.0670,0.4960,0.0670,0,0,1.1776,0,0,0,0,0,0,0,0,0,0.1629,0.1629,0,1.1776,0.1629 ,0.3000 ,0.1629,0.0670,0.0670,0.0670,0.1629,0.0670,0.3000,0.3000,0,0.1629,1.1776 ,0.1629 ,0.3000,0.0670,0.0670,0.0670,0.3000,0.0670,0.1629,0.1629,0,0.3000,0.1629 ,1.1776 ,0.1629,0.0670,0.0670,0.0670,0.1629,0.0670,0.4960,0.4960,0,0.1629,0.3000 ,0.1629 ,1.1776,0.0670,0.0670,0.0670,0.7765,0.0670,0.0670,0.0670,0,0.0670,0.0670 ,0.0670 ,0.0670,1.1776,0.3000,0.3000,0.0670,0.1629,0.0670,0.0670,0,0.0670,0.0670 ,0.0670 ,0.0670,0.3000,1.1776,0.4960,0.0670,0.1629,0.0670,0.0670,0,0.0670,0.0670 ,0.0670 ,0.0670,0.3000,0.4960,1.1776,0.0670,0.1629,0.4960,0.4960,0,0.1629,0.3000 ,0.1629 ,0.7765,0.0670,0.0670,0.0670,1.1776,0.0670,0.0670,0.0670,0,0.0670,0.0670 ,0.0670 ,0.0670,0.1629,0.1629,0.1629,0.0670,1.1776};
     int expect_vh_5_5_size = sizeof(expect_vh_5_5) / sizeof(double);
     test_vh_calculation(0.5,0.5,expect_vh_5_5,expect_vh_5_5_size,epsilon);
-    
+
     double expect_vh_0_5[]= {1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1};
     int expect_vh_0_5_size = sizeof(expect_vh_0_5) / sizeof(double);
     test_vh_calculation(0.0,0.5,expect_vh_0_5,expect_vh_0_5_size,epsilon);
-    
+
     double expect_vh_5_0[]= {1.1776,0.7765,0,0.1629,0.3000,0.1629,0.4960,0.0670,0.0670,0.0670,0.4960,0.0670,0.7765,1.1776,0,0.1629,0.3000,0.1629,0.4960,0.0670,0.0670,0.0670,0.4960,0.0670,0,0,1.1776,0,0,0,0,0,0,0,0,0,0.1629,0.1629,0,1.1776,0.1629,0.3000,0.1629,0.0670,0.0670,0.0670,0.1629,0.0670,0.3000,0.3000,0,0.1629,1.1776,0.1629,0.3000,0.0670,0.0670,0.0670,0.3000,0.0670,0.1629,0.1629,0,0.3000,0.1629,1.1776,0.1629,0.0670,0.0670,0.0670,0.1629,0.0670,0.4960,0.4960,0,0.1629,0.3000,0.1629,1.1776,0.0670,0.0670,0.0670,0.7765,0.0670,0.0670,0.0670,0,0.0670,0.0670,0.0670,0.0670,1.1776,0.3000,0.3000,0.0670,0.1629,0.0670,0.0670,0,0.0670,0.0670,0.0670,0.0670,0.3000,1.1776,0.4960,0.0670,0.1629,0.0670,0.0670,0,0.0670,0.0670,0.0670,0.0670,0.3000,0.4960,1.1776,0.0670,0.1629,0.4960,0.4960,0,0.1629,0.3000,0.1629,0.7765,0.0670,0.0670,0.0670,1.1776,0.0670,0.0670,0.0670,0,0.0670,0.0670,0.0670,0.0670,0.1629,0.1629,0.1629,0.0670,1.1776};
     int expect_vh_5_0_size = sizeof(expect_vh_5_0) / sizeof(double);
     test_vh_calculation(0.5,0.0,expect_vh_5_0,expect_vh_5_0_size,epsilon);
-    
+
     double expect_vh_1_5[]= {NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN,NAN};
     int expect_vh_1_5_size = sizeof(expect_vh_1_5) / sizeof(double);
     test_vh_calculation(1.0,0.5,expect_vh_1_5,expect_vh_1_5_size,epsilon);
@@ -71,68 +71,68 @@ int main(int argc,char *argv[]) {
     int expect_vh_25_25_size = sizeof(expect_vh_25_25) / sizeof(double);
     test_vh_calculation(0.25,0.25,expect_vh_25_25,expect_vh_25_25_size,epsilon);
 
-                    /*Vp=(d2.^tau2).*(1-d2.^(2*initVp))./(1-d2^2);-------------------------------------*/
-                    /*
-                        double A_QQ[q*q];
-                        matrx_mlt(A_QQ,2.00,initVp,q,q);
-                        array_pow(A_QQ,d2,A_QQ,q,q);
-                        matrx_sub(A_QQ,1.00,A_QQ,q,q);
+    /*Vp=(d2.^tau2).*(1-d2.^(2*initVp))./(1-d2^2);-------------------------------------*/
+    /*
+        double A_QQ[q*q];
+        matrx_mlt(A_QQ,2.00,initVp,q,q);
+        array_pow(A_QQ,d2,A_QQ,q,q);
+        matrx_sub(A_QQ,1.00,A_QQ,q,q);
 
-                        double B_QQ[q*q];
-                        array_pow(B_QQ,d2,tau2,q,q);
+        double B_QQ[q*q];
+        array_pow(B_QQ,d2,tau2,q,q);
 
-                        array_mlt(B_QQ,B_QQ,q,q,A_QQ);
-                        array_rdv(B_QQ,B_QQ,q,q,1.00 - d2*d2);
-                    */
-                    /*Vh=Vh./det(Vh)^(1/p);-----------------------------------*/
-                    /*
-                        array_rdv(B_PP,B_PP,p,p,pow(matrx_det(B_PP,p),(1.00/((double) p))));
-                    */
-                    /*Vp=Vp./det(Vp)^(1/q);-----------------------------------*/
-                    /*
-                        array_rdv(B_QQ,B_QQ,q,q,pow(matrx_det(B_QQ,q),(1.00/((double) q))));
-                    */
-                    /*V=kron(Vp,Vh);-----------------------------------*/
-                    /*
-                        double *V_NN;
-                        V_NN = (double *) malloc(n*n*sizeof(double));
-                        kron(V_NN,B_QQ,q,q,B_PP,p,p);
-                    */
-                    /*invV=V\eye(n);-----------------------------------*/
-                    /*
-                        matrx_inv(V_NN,V_NN,n); //saving memory by reusing name
-                    */
-                    /*U=ones(length(X),1);-----------------------------------*/
-                    /*
-                        double A_N[n];
-                        ones(A_N,n,1.00);
-                    */
-                    /*b=(U'*invV*U)\(U'*invV*X);-----------------------------------*/
-                    /*
-                        double B_N[n];
-                        double B_NN[n*n];
-                        tran(B_N,A_N,n,1.00);
-                        matrx_mlt2(B_NN,B_N,1.00,n,V_NN,n,n);
-                        matrx_mlt2(B_N,B_NN,1.00,n,X,n,1.00);
-                        matrx_mlt2(A_N,B_NN,1.00,n,A_N,n,1.00);
-                    */
-                    /*H=X-b;-----------------------------------*/
-                    /*
-                        matrx_sub3(B_N,X,n,1.00,B_N[0] / A_N[0]);
-                    */
-                    /*MSE=(H'*invV*H)/(n-1);-----------------------------------*/
-                    /*
-                        tran(A_N,B_N,n,1.00);
-                        matrx_mlt2(B_NN,A_N,1.00,n,V_NN,n,n);
-                        matrx_mlt2(B_NN,B_NN,1.00,n,B_N,n,1.00);
+        array_mlt(B_QQ,B_QQ,q,q,A_QQ);
+        array_rdv(B_QQ,B_QQ,q,q,1.00 - d2*d2);
+    */
+    /*Vh=Vh./det(Vh)^(1/p);-----------------------------------*/
+    /*
+        array_rdv(B_PP,B_PP,p,p,pow(matrx_det(B_PP,p),(1.00/((double) p))));
+    */
+    /*Vp=Vp./det(Vp)^(1/q);-----------------------------------*/
+    /*
+        array_rdv(B_QQ,B_QQ,q,q,pow(matrx_det(B_QQ,q),(1.00/((double) q))));
+    */
+    /*V=kron(Vp,Vh);-----------------------------------*/
+    /*
+        double *V_NN;
+        V_NN = (double *) malloc(n*n*sizeof(double));
+        kron(V_NN,B_QQ,q,q,B_PP,p,p);
+    */
+    /*invV=V\eye(n);-----------------------------------*/
+    /*
+        matrx_inv(V_NN,V_NN,n); //saving memory by reusing name
+    */
+    /*U=ones(length(X),1);-----------------------------------*/
+    /*
+        double A_N[n];
+        ones(A_N,n,1.00);
+    */
+    /*b=(U'*invV*U)\(U'*invV*X);-----------------------------------*/
+    /*
+        double B_N[n];
+        double B_NN[n*n];
+        tran(B_N,A_N,n,1.00);
+        matrx_mlt2(B_NN,B_N,1.00,n,V_NN,n,n);
+        matrx_mlt2(B_N,B_NN,1.00,n,X,n,1.00);
+        matrx_mlt2(A_N,B_NN,1.00,n,A_N,n,1.00);
+    */
+    /*H=X-b;-----------------------------------*/
+    /*
+        matrx_sub3(B_N,X,n,1.00,B_N[0] / A_N[0]);
+    */
+    /*MSE=(H'*invV*H)/(n-1);-----------------------------------*/
+    /*
+        tran(A_N,B_N,n,1.00);
+        matrx_mlt2(B_NN,A_N,1.00,n,V_NN,n,n);
+        matrx_mlt2(B_NN,B_NN,1.00,n,B_N,n,1.00);
 
-                        free(V_NN);
-                        return B_NN[0] / ((double) n - 1);
-                    */
+        free(V_NN);
+        return B_NN[0] / ((double) n - 1);
+    */
 
-                    //test funct with several values
+    //test funct with several values
 
-                    
+
     test_funct(0.5, 0.5, 0.0101290635, epsilon);
     test_funct(0.0, 0.5, 0.0116081347, epsilon);
     test_funct(0.5, 0.0, 0.0098632064, epsilon);
@@ -172,10 +172,10 @@ int main(int argc,char *argv[]) {
 
     printf("est=\n  %f  %f\n",*(XMIN),*(XMIN+1));
     printf("MSE=\n  %f\n",YNEWLO);
-    if(all_passed == 1){
-      printf("*** Congratulations! No tests FAILED, all PASSED!\n");
+    if(all_passed == 1) {
+        printf("*** Congratulations! No tests FAILED, all PASSED!\n");
     } else {
-      printf("XXX One or more tests FAILED\n");
+        printf("XXX One or more tests FAILED\n");
     }
 
     free(d1_d2);
@@ -219,34 +219,34 @@ void test_vh_calculation(double d1, double d2, double correct_return[], int corr
 
     array_mlt(B_PP,B_PP,p,p,A_PP);
     array_rdv(B_PP,B_PP,p,p,1.00 - d1*d1);
-   
+
     double vh_size = sizeof(B_PP) / sizeof(double);
 
-    if(vh_size == correct_size){
-      for(int i=0; i <correct_size; i++){
-        double actual = B_PP[i];
-        double expect = correct_return[i];
-        double error = actual - expect;
-        if( isnan(actual) && isnan(expect) || (error * error) < epsilon ) {
-            printf("* PASSED");
-        } else {
-            all_passed = 0;
-            printf("X FAILED");
+    if(vh_size == correct_size) {
+        for(int i=0; i <correct_size; i++) {
+            double actual = B_PP[i];
+            double expect = correct_return[i];
+            double error = actual - expect;
+            if( isnan(actual) && isnan(expect) || (error * error) < epsilon ) {
+                printf("* PASSED");
+            } else {
+                all_passed = 0;
+                printf("X FAILED");
+            }
+            printf(" -> test_vh_calculation at %i",i);
+            printf(" -> d1 = %f",d1);
+            printf(" -> d2 = %f",d2);
+            printf(" -> %f returned",actual);
+            printf(" -> %f correct\n",expect);
         }
-        printf(" -> test_vh_calculation at %i",i);
+    } else {
+        all_passed = 0;
+        printf("X FAILED");
+        printf(" -> test_vh_calculation() -- size");
         printf(" -> d1 = %f",d1);
         printf(" -> d2 = %f",d2);
-        printf(" -> %f returned",actual);
-        printf(" -> %f correct\n",expect);
-      }
-    } else {
-      all_passed = 0;
-      printf("X FAILED");
-      printf(" -> test_vh_calculation() -- size");
-      printf(" -> d1 = %f",d1);
-      printf(" -> d2 = %f",d2);
-      printf(" -> %f returned",vh_size);
-      printf(" -> %f correct\n",correct_size);
+        printf(" -> %f returned",vh_size);
+        printf(" -> %i correct\n",correct_size);
     }
 
 }
